f45560342f922dbc3aae7a2234ea360c766909be
anthonybanion <abanon@fi.uba.ar>
2025-12-08 10:10:46 -0300

    Add config

--- backend/vercel_app.py
+++ backend/vercel_app.py
@@ -4,3 +4,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = get_wsgi_application()
+
+# Este nombre es obligatorio para Vercel
+def handler(event, context):
+    return app(event, context)
